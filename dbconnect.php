9f626a47c7904713d415a00d37af9fd8e7c2f333
sea14 <pianocat14@gmail.com>
2013-11-15 10:05:08 -0500

    re-wrote db.connect php because oops

--- dbconnect.php
+++ dbconnect.php
@@ -4,16 +4,14 @@
 	$h = 'server';
 	$u = 'username';
 	$p = 'password';
-	
 	$dbname = 'webdb';
 	
-	//instance of the db
-	$db = new(mysqli($h,$u,$p);
-
-	//select the db, or if not, die and tell us
-	$db->select_db($dbname)
-		or die('Could not make the connection');
 	
-i
-
-?>
+	$mysqli = new mysqli($h, $u, $p, $dbname);
+	
+	if (mysqli_connect_errno () ) {
+		echo "connection failed" .
+			mysqli_connect_errno();
+		exit();
+	}
+?>	
