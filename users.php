13b92416994fbea00742402895b13915aeb5874a
sea14 <pianocat14@gmail.com>
2013-11-17 14:58:06 -0500

    fleshed out users.php a bit

--- users.php
+++ users.php
@@ -1,5 +1,14 @@
 <?php
 	<!--will contain code for accessing users. I think -->
+	class user
+	{
+		public $f_name = ''; //user first name, string
+		public $l_name = ''; //user last name, string
+		public $email = ''; //user email address, string
+		public $coupons = '' //user coupon status, boolean
 
+	}
+
+	//there will be functions here once I figure out what they need to be
 
 ?>
