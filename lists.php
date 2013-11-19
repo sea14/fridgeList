61cc53d58f0b2e223f1624856e63bc023926cb46
sea14 <pianocat14@gmail.com>
2013-11-19 15:04:36 -0500

    replaced item sections with on variable that'll be an array

--- lists.php
+++ lists.php
@@ -6,12 +6,6 @@
 
 		$list_id = '' //list id, pk
 		$name = ''; //list name
-		$vegetables = '' //vegetables array
-		$meats = '' //meats array
-		$fruits = '' //fruits array
-		$grains = '' //grains array
-		$dairy = '' //dairy array
-		$desserts = '' //dessert array
-		$spices = '' //spices/seasonings array
+		$items = ''; //items array
 	}
 ?>
