277ae85052d6b829fe868007d9c87725316b704b
sea14 <pianocat14@gmail.com>
2013-12-05 12:52:44 -0500

    spruced up logout.php. in theory, it's done, but might not be yet

--- logout.php
+++ logout.php
@@ -2,9 +2,7 @@
 
 /*if the user clicks to logout
 * we will destroy the session
-* not sure if this is going to require a fix
-* because it wasn't written for mysqli / prepared statements
-* most likely. */
+ */
 
 
 	session_start();
@@ -13,6 +11,7 @@
 	if (isset_($_COOKIE[session_name()])) {
 
 		setcookie(session_name(), '', time()-42000, '/');
+		unset($_COOKIE[session_name()]);
 	}
 	session_destroy();
 ?>
