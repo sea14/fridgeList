76f00eb5b7a831e45de6434a98a9f19a57802f21
sea14 <pianocat14@gmail.com>
2013-12-06 18:43:13 -0500

    externalized php script for registration

--- register.php
+++ register.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<head>
-<meta charset=UTF-8>
-<title>Fridge List</title>
-<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
-  
-  <body>
+ <?php
 
-  <?php
-
-  
         require 'connect/dbconnect.php';
 
 
@@ -34,39 +24,3 @@
         echo "Done";
 
 ?>
-
-  <div id="logo">
-  <a href="a2.html"><img src="fridgeList_logo.png" alt="Fridge List Logo"/></a>
- <p>&nbsp; &nbsp; an application designed for the busy and hungry! 
-  </div> 
-  
-  <div id="register">
-  <h3><a href="sign_in.php">Sign In</a>/<a href="register.php">Register</a></h3>
-  </div>
-  
-<div id="pagewrap">
-<div class="clear"></div>
-<div id="navigation">
-<a href="a2.html" id="home">Home</a>
-<a href="index.html" id="about">About</a>
-<a href="list.php" id="list">My List</a>
-<a href="recipes.php" id="recipes">Recipes</a>
-
-</div>  
-
-  <div id="main">
-        <p>
-        <form method = "POST" action = "register.php">
-        First Name:<input type="text" name="firstName"><br>
-        Last Name:<input type="text" name="lastName"><br>
-        Email:<input type="text" name="email"><br>
-        Confirm Email:<input type="text" name="confirm"><br>
-        Password:<input type="text" name="password"><br>
-        Coupons?<input type = "checkbox" value = "Yes"><br>
-        </form>
-        </p>
-   </div>
-
-</body>
-
-</html>
