f6f9d4bf05895f45724a06ee8b5b0ea6f41a8c4a
sea14 <pianocat14@gmail.com>
2013-12-05 19:16:36 -0500

    finished security re-write, wrote query

--- register.php
+++ register.php
@@ -36,6 +36,7 @@
 	Last Name:<input type="text" name="lastName"><br>
 	Email:<input type="text" name="email"><br>
 	Confirm Email:<input type="text" name="confirm"><br>
+	Password:<input type = "text" name="password"><br>
 	Coupons?<input type = "checkbox" value = "Yes"><br>
 	</form>
 	</p>
@@ -52,23 +53,31 @@
 		$firstName = htmlentities(strip_tags($_POST['firstName']));
 		$lastName = htmlentities(strip_tags($_POST['lastName']));
 		$email = htmlentities(strip_tags($_POST['email']));
+		$password = sha1(htmlentities(strip_tags($_POST['password'])));
 		$coupons = htmlentities(strip_tags($_POST['coupons']));
 	
 	}else{ //magic_quotes_gpc is off, use addslashes
 
-		$firstName = addslashes(htmlentities(strip_tags($_POST['firstName']));
+		$firstName = addslashes(htmlentities(strip_tags($_POST['firstName'])));
 		$lastName = addslashes(htmlentities(strip_tags($_POST['lastName'])));
 		$email = addslashes(htmlentities(strip_tags($_POST['email'])));
+		$password = sha1(addslashes(htmlentities(strip_tags($_POST['password']))));
 		$coupons = addslashes(htmlentities(strip_tags($_POST['coupons'])));
 
 	}
 
-	//now we write our query
-	
-	printf("%d Row insterted.\n", $stmt->affected_rows);
+	//now we write our query for registering a user
+	$query = 'insert into users (firstName, lastName, email, coupons, password) values'.
+		'($firstName, $lastName, $email, $coupons, $password)';
+
+	$num_rows = mysql_num_rows($result);
+	if($num_rows>0){
+		echo "it worked!";
+
+	}
 
 	//close statement and connection
-	
+	mysql_close($db);	
 
 ?>
 
