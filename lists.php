d45bb9d03a46e87ca2a915915abfc54f85918791
sea14 <pianocat14@gmail.com>
2013-12-07 16:03:16 -0500

    put html in lists.php:

--- lists.php
+++ lists.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<meta charset=UTF-8>
+    <title>Fridge List</title>
+	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+  </head>
+  
+  <body>
+
 <?php
 
 	<!--class for the lists that users create for themselves -->
@@ -8,4 +16,35 @@
 		$name = ''; //list name
 		$items = ''; //items array
 	}
+
+	echo "Here are your lists," . $firstName . "!";
+
 ?>
+
+ <div id="logo">
+  <a href="a2.html"><img src="fridgeList_logo.png" alt="Fridge List Logo"></a>
+ <p>&nbsp; &nbsp; an application designed for the busy and hungry! 
+  </div> 
+  
+  <div id="register">
+	<h3><a href="sign_in.php">Sign In</a>/<a href="register.php">Register</a></h3>
+	</div>
+	
+<div id="pagewrap">
+<div class="clear"></div>
+<div id="navigation">
+<a href="a2.html" id="home">Home</a>
+<a href="index.html" id="about">About</a>
+<a href="list.php" id="list">My List</a>
+<a href="recipes.php" id="recipes">Recipes</a>
+
+</div>	
+
+	<div id="main">
+	<p><a href="register.php">Register</a> or <a href="sign_in.php">sign in</a> to start creating your grocery lists!<br /></p><br />
+
+   </div>
+  </div>
+   </body>
+ </html>
+
