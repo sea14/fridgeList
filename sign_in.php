00d6163134b37d90ce248bc7b3cad4ebb075b629
sea14 <pianocat14@gmail.com>
2013-12-08 16:56:32 -0500

    more sprucing up, redid js script

--- sign_in.php
+++ sign_in.php
@@ -7,6 +7,15 @@ session_start();
   //check if username and password are set. if so, try to log user in
   if(isset($_POST['email']) && isset($_POST['password']) ){
 
+
+    //another email existence validation check
+     $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+
+          $emailaddress = $_POST['email'];
+
+          if (preg_match($pattern, $emailaddress) === 1) {
+
+
     $stmt = $mysqli->prepare("SELECT email FROM users WHERE email=? AND password=?");
 
     $email = $_POST['email'];
@@ -19,9 +28,12 @@ session_start();
     $stmt->bind_result($email, $password);
     $stmt->fetch(); 
 
+
   
   $stmt->close();
   mysqli_close($mysqli);
+
+  }
 
 }
 
@@ -32,37 +44,20 @@ session_start();
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
   
 
-  <!--<script type="text/javascript">
-  $(function() {
-        var action = '';
-        var form_data = '';
-        $('#submit').click(function () {
-                action = $("#loginForm").attr("action");
-                form_data = {
-                email: $("#email").val(),
-                password: $("#password").val(),
-                is_ajax: '1'};
+  <script type="text/javascript">
+  $("#loginForm").submit(function(e){
+    e.preventDefault();
+    $.post('sign_in.php', $(this).serialize(), function(data){
 
-        $.ajax({
-                type: 'POST',
-                url: action,
-                data: form_data,
-                success: function(response) {
-                        if(response == 'success') {
-                                $("#loginForm").slideUp('slow', function() {
-                                        $("#message").html('<p class="success">You have logged in successfully!</p>');
-                                });
-                        } else {
-                                $("#message").html('<p class = error"> Invalid username and/or password.</p>');
-                        }
-                }
-        });
-        return false;
-    })
+      $("#showusers").load("model-views/user.php");
+      $("#userinfo").text(data);
+      $("#loginForm").remove();
 
-}); 
+     });
 
-  </script> -->
+ });
+
+  </script> 
   </head>
   <body>
 
@@ -97,7 +92,8 @@ session_start();
         Password: <input type="password" id="password"  name="password"><br>
    <input type="submit" id="submit" value="submit">
 
-   <div id="userinfo"><?php echo $email; ?>
+   <div id="userinfo"><? echo $_SESSION["email"] ?> </div>
+
 
    </div>
   </div>
