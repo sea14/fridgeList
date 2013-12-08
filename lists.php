986a083d43c587aa064d85234b2c086da89ae8e3
sea14 <pianocat14@gmail.com>
2013-12-08 08:16:31 -0500

    redid recipes and lists to fit more of a model system. doubt they're right atm, but hopefully will be later

--- lists.php
+++ lists.php
@@ -1,42 +1,45 @@
-<!DOCTYPE html>
-<meta charset=UTF-8>
-    <title>Fridge List</title>
-	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-  </head>
-  
-  <body>
-
 <?php
-	//will write code later. very tired of this. ugh.
-
-	echo "Here are your lists," . $firstName . "!";
-
-?>
-
- <div id="logo">
-  <a href="a2.html"><img src="fridgeList_logo.png" alt="Fridge List Logo"></a>
- <p>&nbsp; &nbsp; an application designed for the busy and hungry! 
-  </div> 
-  
-  <div id="register">
-	<h3><a href="sign_in.php">Sign In</a>/<a href="register.php">Register</a></h3>
-	</div>
+	//this contains code for accessing lists
 	
-<div id="pagewrap">
-<div class="clear"></div>
-<div id="navigation">
-<a href="a2.html" id="home">Home</a>
-<a href="index.html" id="about">About</a>
-<a href="list.php" id="list">My List</a>
-<a href="recipes.php" id="recipes">Recipes</a>
 
-</div>	
+	//get user by id
+	function get_list_by_id($id)
+	{
+		$lists_list = array();
 
-	<div id="main">
-	<p><a href="register.php">Register</a> or <a href="sign_in.php">sign in</a> to start creating your grocery lists!<br /></p><br />
 
-   </div>
-  </div>
-   </body>
- </html>
+		return $lists_info;
 
+	}
+
+	function_get_lists_list()
+	{
+	
+
+		return $lists_list
+	}
+
+	$user_url = array("get_lists_list", "get_list");
+
+	$value = "An error has occurred";
+
+	if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+	{
+	switch ($_GET["action])
+	 {
+	   case = "get_lists_list";
+		$value = get_lists_list();
+		break;
+	   case "get_list";
+		if (isset($_GET["id"]))
+		   $value = get_list_by_id($_GET["id"]);
+		else
+			$value = "Missing argument";
+		break;
+	}
+
+}
+
+	//return json array
+	exit(json_encode($value));
+?>
--- recipes.php
+++ recipes.php
@@ -1,62 +1,45 @@
-<!DOCTYPE html>
-<meta charset=UTF-8>
-    <title>Fridge List</title>
-	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-  </head>
-  
-  <body>
-
 <?php
+	//this contains code for accessing recipes
+	
 
-//recipes.php holds a user's created recipes. if a user isn't logged in, it asks them to do so -->
-
-	class recipe
+	//get user by id
+	function get_recipe_by_id($id)
 	{
-		public $recipe_id = ''; //recipe id, pk
-		public $name = ''; //recipe name
-		public $url = ''; //recipe url
+		$recipe_list = array();
+
+
+		return $recipe_info;
+
 	}
 
-	//functions will go below here, not sure what they'll be yet
-
-
-
-	if(isset($_SESSION['valid_user'])) {
-		//user has logged in
-	echo 'Welcome, ' . $_SESSION['valid_user'] . '<br>' . 'Your recipes are listed below.';
-	}else{
-			echo "You're not logged in -- would you please do so below?";
-	}
+	function_get_recipe_list()
+	{
 	
+
+		return $recipe_list
+	}
+
+	$recipe_url = array("get_recipe_list", "get_recipe");
+
+	$value = "An error has occurred";
+
+	if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+	{
+	switch ($_GET["action])
+	 {
+	   case = "get_recipe_list";
+		$value = get_recipe_list();
+		break;
+	   case "get_recipe";
+		if (isset($_GET["id"]))
+		   $value = get_recipe_by_id($_GET["id"]);
+		else
+			$value = "Missing argument";
+		break;
+	}
+
+}
+
+	//return json array
+	exit(json_encode($value));
 ?>
-
- <div id="logo">
-  <a href="a2.html"><img src="fridgeList_logo.png" alt="Fridge List Logo"></a>
- <p>&nbsp; &nbsp; an application designed for the busy and hungry! 
-  </div> 
-  
-  <div id="register">
-	<h3><a href="sign_in.php">Sign In</a>/<a href="register.php">Register</a></h3>
-	</div>
-	
-<div id="pagewrap">
-<div class="clear"></div>
-<div id="navigation">
-<a href="a2.html" id="home">Home</a>
-<a href="index.html" id="about">About</a>
-<a href="lists.php" id="list">My List</a>
-<a href="recipes.php" id="recipes">Recipes</a>
-
-</div>	
-
-	<div id="main">
-	<p><a href="register.php">Register</a> or <a href="sign_in.php">sign in</a> to start creating your grocery lists!<br /></p><br />
-	<form method = "post" action = "sign_in.php">
-        Email: <input type="text" name="login_user"><br>
-        Password: <input type="password" name="login_pass"><br>
-  <input type="submit" value="submit">
-   </div>
-  </div>
-   </body>
- </html>
-
