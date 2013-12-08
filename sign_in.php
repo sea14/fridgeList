d4b99fcde6fc68dc0ac933e802d5ed8a4ef04027
sea14 <pianocat14@gmail.com>
2013-12-08 09:53:44 -0500

    sign_in.php script may be done, not sure yet.

--- sign_in.php
+++ sign_in.php
@@ -12,12 +12,26 @@
 
 		//create query to check for existence of user input in the db
 
-	}else{
+
+		if($stmt = $mysqli->prepare("SELECT * FROM users WHERE email=? AND password=?")){
+			//bind parameters
+			$stmt->bind_param('ss', $email, $password);
+			$stmt->execute();
+			$stmt->bind_result($email, $password);
+
+			if( TRUE !== $stmt->fetch() ){
+
+				$return = FALSE;
+
+			}
+
+		$stmt->close();
+
+		return $return;
+
+		$mysqli->close();
 
 
-		echo "Invalid user login information.";
-
-	}
 
 ?>
 
