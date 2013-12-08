9b807e864c7e198963620f8482d615f35de635e2
sea14 <pianocat14@gmail.com>
2013-12-08 07:34:40 -0500

    wrote basics for users, probably not done yet

--- users.php
+++ users.php
@@ -1,15 +1,45 @@
 <?php
-	<!--will contain code for accessing users. I think -->
-	class user
+	//this contains code for accessing users
+	
+
+	//get user by id
+	function get_user_by_id($id)
 	{
-		public $user_id = ''; //user id, pk
-		public $f_name = ''; //user first name, string
-		public $l_name = ''; //user last name, string
-		public $email = ''; //user email address, string
-		public $coupons = '' //user coupon status, boolean
+		$users_list = array();
+
+
+		return $users_info;
 
 	}
 
-	//check to see if user is in db
+	function_get_user_list()
+	{
+	
 
+		return $user_list
+	}
+
+	$user_url = array("get_user_list", "get_user");
+
+	$value = "An error has occurred";
+
+	if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+	{
+	switch ($_GET["action])
+	 {
+	   case = "get_user_list";
+		$value = get_user_list();
+		break;
+	   case "get_user";
+		if (isset($_GET["id"]))
+		   $value = get_user_by_id($_GET["id"]);
+		else
+			$value = "Missing argument";
+		break;
+	}
+
+}
+
+	//return json array
+	exit(json_encode($value));
 ?>
