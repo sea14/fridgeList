342cdb20e4dd2e63a43f65fb7b724f2067c9c602
sea14 <pianocat14@gmail.com>
2013-12-09 10:40:38 -0500

    added html components to controllers

--- controller-scripts/display-recipes.php
+++ controller-scripts/display-recipes.php
@@ -1,6 +1,62 @@
 //displays a user's recipes
 <?php
-
+ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
+session_start();
 
 
 ?>
+<!DOCTYPE html>
+<meta charset=UTF-8>
+    <title>Fridge List</title>
+	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  
+
+  <script type="text/javascript">
+  $("#loginForm").submit(function(e){
+    e.preventDefault();
+    $.post('sign_in.php', $(this).serialize(), function(data){
+
+      $("#showusers").load("model-views/user.php");
+      $("#userinfo").text(data);
+      $("#loginForm").remove();
+
+     });
+
+ });
+
+  </script> 
+  </head>
+  <body>
+
+
+
+
+ <div id="logo">
+  <a href="a2.php"><img src="fridgeList_logo.png" alt="Fridge List Logo"></a>
+ <p>&nbsp; &nbsp; an application designed for the busy and hungry! 
+  </div> 
+  
+  <div id="register">
+	<h3><a href="sign_in.php">Sign In</a>/<a href="register.php">Register</a></h3>
+	</div>
+	
+<div id="pagewrap">
+<div class="clear"></div>
+<div id="navigation">
+<a href="a2.php" id="home">Home</a>
+<a href="index.html" id="about">About</a>
+<a href="lists.php" id="list">My List</a>
+<a href="recipes.php" id="recipes">Recipes</a>
+
+</div>	
+
+	<div id="main">
+	<p><a href="register.php">Register</a> or <a href="sign_in.php">sign in</a> to start creating your grocery lists!<br /></p><br />
+
+  </div>
+
+
+
+   </body>
+ </html>
